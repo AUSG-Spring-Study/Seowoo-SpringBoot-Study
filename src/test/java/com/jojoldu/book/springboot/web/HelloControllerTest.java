8e6b88672ffebf4826b9500bafe0370873ed5c45
dltjdn <hana40004@naver.com>
2022-09-15 21:17:56 +0900

    [2.2] springBootVersion 변경

--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -10,8 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import com.jojoldu.book.springboot.web.HelloController;
+
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
