5d621156aaaa7f3407894bde574db0f4df46d3b2
dltjdn <hana40004@naver.com>
2022-11-03 20:00:39 +0900

    [5.6] 네이버 로그인

--- src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -3,6 +3,7 @@ package com.jojoldu.book.springboot.config.auth.dto;
 import com.jojoldu.book.springboot.domain.user.Role;
 import com.jojoldu.book.springboot.domain.user.User;
 import java.util.Map;
+import java.util.Objects;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -24,6 +25,9 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if("naver".equals(registrationId)){
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -33,6 +37,18 @@ public class OAuthAttributes {
             .email((String) attributes.get("email"))
             .picture((String) attributes.get("picture"))
             .attributes(attributes)
+            .nameAttributeKey(userNameAttributeName)
+            .build();
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes){
+        Map<String, Object> response = (Map<String, Object>)attributes.get("response");
+
+        return OAuthAttributes.builder()
+            .name((String) response.get("name"))
+            .email((String) response.get("email"))
+            .picture((String) response.get("profile_image"))
+            .attributes(response)
             .nameAttributeKey(userNameAttributeName)
             .build();
     }
